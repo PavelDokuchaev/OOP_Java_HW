446ddaaf02d5b4cbfaf8e3c8a87d93d047cf6714
dokuchaev.pa <dokuchaev.pa@gmail.com>
2023-03-04 20:30:49 +0400

    hw7

---
+++ homework7/EmpolyeeMain.java
@@ -0,0 +1,26 @@
+package homework7;
+
+/*
+Создать класс Сотрудник (Employee) с полями:
+Имя, Фамилия (String)
+Название отдела (String)
+Зарплата (double)
+Реализовать equals и hashCode, который работает только с именем и фамилией 
+*/
+
+public class EmpolyeeMain {
+    public static void main(String[] args) {
+        Employee employee1 = new Employee("Denis", "Nazarov", "Accaunting", 50000);
+        Employee employee2 = new Employee("Denis", "Nazarov", "Legal Department", 65000);
+        Employee employee3 = new Employee("Ivan", "Ivanov", "Legal Department", 65000);
+
+        System.out
+                .println("e1: " + employee1 + "\ne2: " + employee2 + "\nequals: " + employee1.equals(employee2) + "\n");
+        System.out
+                .println("e2: " + employee2 + "\ne3: " + employee3 + "\nequals: " + employee2.equals(employee3) + "\n");
+
+        System.out.println("hashCode:");
+        System.out.println(employee1.hashCode() == employee2.hashCode());
+        System.out.println(employee2.hashCode() == employee3.hashCode());
+    }
+}
