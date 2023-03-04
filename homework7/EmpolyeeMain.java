2bcb804aa2aaffc00d868805612ac589a107794a
dokuchaev.pa <dokuchaev.pa@gmail.com>
2023-03-04 20:45:10 +0400

    hw7_1

--- homework7/EmpolyeeMain.java
+++ homework7/EmpolyeeMain.java
@@ -15,11 +15,13 @@ public class EmpolyeeMain {
         Employee employee3 = new Employee("Ivan", "Ivanov", "Legal Department", 65000);
 
         System.out
-                .println("e1: " + employee1 + "\ne2: " + employee2 + "\nequals: " + employee1.equals(employee2) + "\n");
+                .println("e1: " + employee1 + "\ne2: " + employee2 + "\nСравнение equals: "
+                        + employee1.equals(employee2) + "\n");
         System.out
-                .println("e2: " + employee2 + "\ne3: " + employee3 + "\nequals: " + employee2.equals(employee3) + "\n");
+                .println("e2: " + employee2 + "\ne3: " + employee3 + "\nСравнение equals: "
+                        + employee2.equals(employee3) + "\n");
 
-        System.out.println("hashCode:");
+        System.out.println("Сравнение hashCode:");
         System.out.println(employee1.hashCode() == employee2.hashCode());
         System.out.println(employee2.hashCode() == employee3.hashCode());
     }
